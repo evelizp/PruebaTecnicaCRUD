83f10cc42d30249fb6160b4dc3a7c74c82ddf92d
evelizp <erickveliz86@gmail.com>
2022-12-16 18:58:57 -0500

    Agregando Proyecto

---
+++ src/main/java/com/evelizp/springbootcrud/model/Cliente.java
@@ -0,0 +1,68 @@
+package com.evelizp.springbootcrud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nombres;
+	
+	@Column
+	private String apellidos;
+	
+	@Column
+	private String direccion;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String celular;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+}
